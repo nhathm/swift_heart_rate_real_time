38a5a5558a33706c023e1f28f92c21169977c64c
NhatHM <nhathm01247@gmail.com>
2019-08-28 16:16:25 +0700

    Add sample

---
+++ ElecDemoTests/ElecDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ElecDemoTests.swift
+//  ElecDemoTests
+//
+//  Created by NhatHM on 8/9/19.
+//  Copyright © 2019 GST.PID. All rights reserved.
+//
+
+import XCTest
+@testable import ElecDemo
+
+class ElecDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
